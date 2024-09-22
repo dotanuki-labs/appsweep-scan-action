b992056ccf98cb3c5384836e06187188fa70b28e
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2024-09-22 09:42:52 +0200

    Implements core GHA logic (#7)
    * Implements core GHA logic
    
    * Improves error message

---
+++ e2e/pocketcasts-android.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Copyright 2024 Dotanuki Labs
+# SPDX-License-Identifier: MIT
+
+set -euo pipefail
+
+readonly repo="Automattic/pocket-casts-android"
+readonly version="7.72"
+readonly asset="app-7.72.apk"
+readonly download_url="https://github.com/$repo/releases/download/$version/$asset"
+readonly package="pocket-casts-android.apk"
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${script_dir%/*}"
+actual_dir=$(pwd)
+
+rm -rf "$actual_dir/.tmp" && mkdir "$actual_dir/.tmp"
+curl -fsSL -o "$actual_dir/.tmp/$package" -C - "$download_url"
+src/main.sh "$actual_dir/.tmp/$package"
--- src/main.sh
+++ src/main.sh
@@ -4,4 +4,97 @@
 
 set -e
 
-echo "Hello World"
+readonly install_location="$HOME/bin"
+readonly guardsquare="$install_location/guardsquare"
+readonly installer_url="https://platform.guardsquare.com/cli/install.sh"
+
+readonly archive="$1"
+readonly extras="$2"
+
+require_archive() {
+    if [[ -z "$archive" ]]; then
+        echo "✗ ERROR : expecting an 'archive' input"
+        exit 1
+    fi
+
+    if [[ ! -f "$archive" ]]; then
+        echo "✗ ERROR : '$archive' not found"
+        exit 1
+    fi
+}
+
+require_r8_or_proguard_mappings() {
+    if [[ ! -f "$extras" ]]; then
+        echo "✗ ERROR : '$extras' R8/proguard mapping file not found"
+        exit 1
+    fi
+}
+
+require_dsyms_folder() {
+    if [[ ! -d "$extras" ]]; then
+        echo "✗ ERROR : '$extras' folder not found"
+        exit 1
+    fi
+}
+
+install_guardsquare_cli() {
+    mkdir -p "$install_location"
+    curl -sSL "$installer_url" | sh -s -- -y --bin-dir "$install_location"
+}
+
+execute_android_scan() {
+    local scan_id
+
+    if [[ -z "$extras" ]]; then
+        echo "Scanning standalone archive : $archive"
+        install_guardsquare_cli
+        scan_id=$("$guardsquare" scan "$archive" --commit-hash "$GITHUB_SHA" --format "{{.ID}}")
+    else
+        require_r8_or_proguard_mappings
+        echo "Scanning archive     : $archive"
+        echo "R8/Proguard mappings  : $extras"
+        install_guardsquare_cli
+        scan_id=$("$guardsquare" scan "$archive" --mapping-file "$extras" --commit-hash "$GITHUB_SHA" --format "{{.ID}}")
+    fi
+
+    "$guardsquare" scan summary --wait-for static "$scan_id" --format json | jq
+}
+
+execute_ios_scan() {
+    local scan_id
+
+    if [[ -z "$extras" ]]; then
+        echo "Scanning standalone archive : $archive"
+        install_guardsquare_cli
+        scan_id=$("$guardsquare" scan "$archive" --commit-hash "$GITHUB_SHA" --format "{{.ID}}")
+    else
+        require_dsyms_folder
+        echo "Scanning archive : $archive"
+        echo "dsyms location    : $extras"
+        install_guardsquare_cli
+        scan_id=$("$guardsquare" scan "$archive" --dsym "$extras" --commit-hash "$GITHUB_SHA" --format "{{.ID}}")
+    fi
+
+    "$guardsquare" scan summary --wait-for static "$scan_id" --format json | jq
+}
+
+require_archive
+
+case "$archive" in
+*.apk | *.aab)
+    execute_android_scan
+    ;;
+*.xcarchive | *.ipa)
+    execute_ios_scan
+    ;;
+*)
+    echo "Error: unsupported archive → $archive"
+    echo
+    echo "Supported archives :"
+    echo
+    echo "- Android : .aab, .apk"
+    echo "- iOS : .ipa, .xcarchive"
+    echo
+    exit 1
+    ;;
+esac
